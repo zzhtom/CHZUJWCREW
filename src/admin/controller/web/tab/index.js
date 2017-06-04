73e3f4c6fdbf8a60a0becea3d4913bc95f37689d
unknown <>
2017-06-04 23:06:50 +0800

    后台代码

--- src/admin/controller/web/tab/index.js
+++ src/admin/controller/web/tab/index.js
@@ -13,9 +13,28 @@ export default class extends Base {
   }
   async updateAction() {
     //auto render template file index_update.html
-    let adminInfor = await this.model('admin').where({ username: "admin" }).find();
-    console.log(adminInfor);
+    let userInfo = await this.session('userInfo');
+    if (think.isEmpty(userInfo)) {
+      return this.fail('用户信息错误');
+    }
+    if(this.get('msg') !== undefined)
+      this.assign('msg',this.get('msg'));
+    let adminInfor = await this.model('admin').where({ username: userInfo.username }).find();
     this.assign('data', adminInfor);
     return this.display();
+  }
+  async adminAction() {
+    //auto render template file index_update.html
+    let userInfo = await this.session('userInfo');
+    if (think.isEmpty(userInfo)) {
+      return this.fail('用户信息错误');
+    }
+    let adminInfor = this.post();
+    let model = this.model('admin');
+    let affectedRows = await model.where({ username: userInfo.username }).update(adminInfor);
+    if(affectedRows !=1)
+      return this.fail('更新' + userInfo.username + '信息失败');
+    let msg="modify success!"
+    return this.redirect('update?msg=' + msg);
   }
 }
