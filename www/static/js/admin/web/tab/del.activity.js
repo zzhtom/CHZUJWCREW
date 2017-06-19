8a386a1c51db7f020f1934860152bf15696e4620
zzhtom <361758650@qq.com>
2017-06-19 10:23:08 +0800

    Update del.activity.js

--- www/static/js/admin/web/tab/del.activity.js
+++ www/static/js/admin/web/tab/del.activity.js
@@ -7,13 +7,14 @@ $(document).ready(function () {
         } else{
             $("input[name='checkbox']").prop("checked", false);//全选 
         }
-    });
-    if ($.browser.msie) {
-        $('input:checkbox').click(function () {
+        if ($.browser.msie) {
+            $('input:checkbox').click(function () {
             this.blur();
             this.focus();
         })
     }
+    });
+
     $('.singleDel').click(function () {
         //  var tr = $(this);
         //  $(this).remove();
@@ -39,6 +40,39 @@ $(document).ready(function () {
                     // window.location.reload();
                 } else {
                     alert("删除活动标题为:" + data.title + "的活动信息失败！");
+                }
+            },
+            error: function () {
+                alert("网络异常！");
+            }
+        });
+    });
+     $('.allDel').click(function () {
+        //  var tr = $(this);
+        //  $(this).remove();
+        // $('#showActivity tr:first').remove();
+//         var $tr = $(this).parent().parent();
+        var $trs = $('input[name=checkbox]').parent().parent().parent();
+        var $td = $tr.find("td");
+        $.ajax({
+            url: 'delactivity',
+            data: {
+                aid: $.trim($td.eq(1).text()),
+                title: $.trim($td.eq(3).text()),
+                mdname: $.trim($td.eq(4).text()),
+            },
+            type: 'post',
+            cache: false,
+            dataType: 'json',
+            success: function (data) {
+                // var jsonData = JSON.stringify(data);
+                if (data.success) {
+                    // $('#tips').html('');
+                    alert("批量删除的活动信息成功！");
+                    $tr.remove();
+                    // window.location.reload();
+                } else {
+                    alert("批量删除的活动信息失败！");
                 }
             },
             error: function () {
