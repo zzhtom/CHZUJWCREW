e8a5b82171bc3b8c22b237d50f64d040228c32c3
zzhtom <361758650@qq.com>
2017-08-13 17:33:01 +0800

    update home

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -5,6 +5,7 @@
 export default {
   //key: value
   timeout: 30,
+  port: 50,
   // resource_on: true, //是否开启静态资源解析功能
   // resource_reg: /^(static\/|[^\/]+\.(?!js|html)\w+$)/, //判断为静态资源请求的正则
   cache: {
